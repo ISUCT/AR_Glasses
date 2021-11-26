acb7b98f81d6432c2c0d3ae7c2fac82ac008f337
unknown <Administrator@o82.rp.ru>
2021-11-26 22:46:49 +0300

    Get, split and display name

--- Arduino-src/main/main.ino
+++ Arduino-src/main/main.ino
@@ -4,15 +4,18 @@
 
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
+String currentString = "";
+String name = "";
+String surname = "";
 
 void setup() {
   Serial.begin(9600);
+  display.setRotation(2);
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
@@ -24,10 +27,26 @@ void setup() {
   }
 }
 
-String GetSerialData()
+void GetSerialData()
 {
-  String data = "Hello World!";//Serial.read();
-  return data;
+  if (Serial.available() != 0)
+  {
+    String data = Serial.readString();
+    if (data == "null")
+    {
+      currentString = " ";
+    }
+    else
+    {
+      currentString = data;
+    }
+  }
+}
+void SplitName()
+{
+  int deliIndex = currentString.indexOf(' ');
+  name = currentString.substring(0, deliIndex);
+  surname = currentString.substring(deliIndex + 1);
 }
 
 void DrawText(byte x, byte y, String text, bool haveClear)
@@ -37,13 +56,15 @@ void DrawText(byte x, byte y, String text, bool haveClear)
     display.clearDisplay();
   }
   display.setCursor(x, y);
-  display.println(text);
+  display.println(name);
+  display.println(surname);
   display.display();
 }
 
 void loop()
 {
-  String data = GetSerialData();
-  DrawText(0, 0, data, true);
+  GetSerialData();
+  SplitName();
+  DrawText(0, 0, currentString, true);
 
 }
