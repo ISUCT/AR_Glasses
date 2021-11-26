30e8d0cbcd92d467116756a2c57864728ae8116b
Egor Dunaev <egodunaev@gmail.com>
2021-11-27 02:46:20 +0300

    added try/except for serial work for safe debugging

--- pi/helpers/serial_helper.py
+++ pi/helpers/serial_helper.py
@@ -9,14 +9,23 @@ serial_comm.port = '/dev/cu.usbserial-110'
 serial_comm.timeout = 10 #in seconds
 
 def send_data_to_arduino(text_data):
-    if serial_comm.isOpen():
-        sleep(2)
-        serial_comm.write(bytearray(text_data, 'utf-8'))
-        serial_comm.flush()
-        serial_comm.close()
+    try:
+        if serial_comm.isOpen():
+            sleep(2)
+            serial_comm.write(bytearray(text_data, 'utf-8'))
+            serial_comm.flush()
+            serial_comm.close()
+    except SerialException as ex:
+        print(ex)
 
 def open_serial_connection():
-    serial_comm.open()
+    try:
+        serial_comm.open()
+    except SerialException as ex:
+        print(ex)
 
 def close_serial_connection():
-    serial_comm.close()
+    try:
+        serial_comm.close()
+    except SerialException as ex:
+        print(ex)
