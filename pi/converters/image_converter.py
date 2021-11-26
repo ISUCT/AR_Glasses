ad04116a530219a447673a3c9bad41f99719099a
Egor Dunaev <egodunaev@gmail.com>
2021-11-26 21:06:25 +0300

    Improved image encoding

--- pi/converters/image_converter.py
+++ pi/converters/image_converter.py
@@ -6,7 +6,9 @@ def convert_image_to_base64(path=None, img=None):
         image = open(path, 'rb')
         image_read = image.read()
         encoded_image = base64.b64encode(image_read)
+        encoded_image = encoded_image.decode('utf-8')
     if img is not None:
         etval, buffer = cv2.imencode('.jpg', img)
         encoded_image = base64.b64encode(buffer)
+        encoded_image = encoded_image.decode('utf-8')
     return encoded_image
